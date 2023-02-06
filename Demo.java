11a8b2e0b050b0b6bf1b64aa93defaf5e94f514d
altynaykarzhaubay <altynaikarzhaubai@gmail.com>
2023-02-06 12:31:58 +0600

    квадраттық функцияларды құрдық

--- Demo.java
+++ Demo.java
@@ -1,5 +1,14 @@
 public class Demo{
     public static void main(String[] args) {
         System.out.println("Salem.Alem!");
+
+        int a = 12 ;
+        int res ;
+        res = kvadrat(a) ;
+        System.out.print(res);
+    }
+
+    public static int kvadrat(int a){
+        return a*a;
     }
 }
--- code.py
+++ code.py
@@ -1,4 +1,12 @@
 #Salem, Alem сөзін шығару
 print("Salem, Alem")
+
+a=12
+res = kvadrat(a)
+print("a = ", res)
+
 def title():
     print("TITLE")
+
+def kvadrat(a):
+    return a*a
